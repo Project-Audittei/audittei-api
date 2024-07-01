076a32efcfb7470bb7d9560c48532788271a1419
Fernando Avila <fernandoavilajunior@gmail.com>
2024-07-01 14:57:01 -0300

    Redefinindo CORS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,7 +17,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->appendToGroup('api', [ValidadorRequisicaoMiddleware::class]);
-        $middleware->appendToGroup('api', [Cors::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         $exceptions->render(function (Exception $ex) {
